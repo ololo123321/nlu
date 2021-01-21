9ddf83300d0af9d55b6e404022baafb977208577
ololo123321 <vitkadylin@gmail.com>
2021-01-21 08:39:30 +0000

    fixing .gitignore

--- evaluate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-answers_dir=/home/datascientist/relation-extraction/data/bankruptcy/test
-predictions_dir=/home/datascientist/relation-extraction/data/bankruptcy/predictions
-#predictions_dir=/home/datascientist/relation-extraction/data/bankruptcy/predictions_rule_based
-
-python3 evaluate.py \
-    --answers_dir=${answers_dir} \
-    --predictions_dir=${predictions_dir}
--- split_data.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-data_dir=/home/datascientist/relation-extraction/data/bankruptcy/v2
-
-python3 split_data.py --data_dir=${data_dir}
